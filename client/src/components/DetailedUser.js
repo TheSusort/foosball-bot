f764c693a418a32e9f831284dcefb99d6f35380e
Kenneth Susort <kenneth.susort@visma.com>
2021-10-19 14:59:46 +0200

    [FEATURE] More loading fixes

--- client/src/components/DetailedUser.js
+++ client/src/components/DetailedUser.js
@@ -13,7 +13,7 @@ const UserList = (props) => {
         return (
             <div className={"max-w-7xl mx-auto mb-8 bg-white p-4 rounded rounded-lg shadow-lg relative"}>
                 <Link className={"absolute left-4 text-xs md:text-base"} to={"/"}>Back</Link>
-                <h2 className={"text-center text-sm md:text-2xl name pt-5 h-10"}>{user.name}</h2>
+                <h2 className={"text-center text-sm md:text-2xl name pt-5"}>{user.name ?? "Username"}</h2>
 
                 <div className="flex border-t border-gray-600 text-xs md:text-base overflow-x-auto">
                     <div
