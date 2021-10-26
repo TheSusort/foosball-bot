2a3c56184efbcbe955455d666237805d61ec291a
Kenneth Susort <kenneth.susort@visma.com>
2021-10-26 09:06:38 +0200

    [FEATURE] use downsize gifs

--- functions/foosball/services/giphy.js
+++ functions/foosball/services/giphy.js
@@ -6,15 +6,9 @@ const gf = new GiphyFetch(GIPHY_API_KEY);
 const gifSearch = async (term) => {
     try {
         const limit = 25;
-        let onlyBigGifs = true;
         const result = await gf.search(term, {sort: "relevant", limit: limit});
-        while (onlyBigGifs) {
-            const randomGif = result.data[Math.floor(Math.random() * limit)];
-            if (randomGif.images.original.size < 3000000) {
-                onlyBigGifs = false;
-                return randomGif.bitly_url;
-            }
-        }
+        const randomGif = result.data[Math.floor(Math.random() * limit)];
+        return randomGif.images.downsized.url;
     } catch (error) {
         console.error("search", error);
     }
